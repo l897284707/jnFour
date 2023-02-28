a4d97ecef9db83f992d621424399dc75e1524fa7
l897284707 <897284707@qq.com>
2023-02-28 15:28:21 +0800

    springCloud's Eureka

--- day06-spring-cloud-consumer/src/main/java/com/qfedu/cloud/controller/ConsumerController.java
+++ day06-spring-cloud-consumer/src/main/java/com/qfedu/cloud/controller/ConsumerController.java
@@ -3,8 +3,12 @@ package com.qfedu.cloud.controller;
 
 import com.qfedu.cloud.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
 
 /**
  * @version java version 1.8
@@ -13,13 +17,21 @@ import org.springframework.web.client.RestTemplate;
  * @date: 2023-02-28 09:47
  */
 @RestController
-@RequestMapping("consumer/user")
+@RequestMapping("consumer")
 public class ConsumerController {
     @Autowired
     private RestTemplate restTemplate;
-    @GetMapping
-    @ResponseBody
+
+    //服务客户端
+    @Autowired
+    private DiscoveryClient discoveryClientl;
+    @GetMapping("user")
     public User getUser(@RequestParam("id") Integer id){
+        //根据注册的实例名称来获取实例
+        List<ServiceInstance> instances = discoveryClientl.getInstances("service-provider");
+
+
+
         //远程调用接口服务
         String url = "http://localhost:8081/user/"+id;
         User forObject = restTemplate.getForObject(url, User.class);

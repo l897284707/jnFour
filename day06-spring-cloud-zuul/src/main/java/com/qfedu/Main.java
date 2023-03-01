035e20988edc5ff5d5b279c8261fc8d37576f89c
LiJH <897284707@qq.com>
2023-03-01 14:30:55 +0800

    springcloud-zuul

---
+++ day06-spring-cloud-zuul/src/main/java/com/qfedu/Main.java
@@ -0,0 +1,13 @@
+package com.qfedu;
+
+/**
+ * TODO 类描述
+ *
+ * @author LiJH.
+ * @date ${DATE} ${TIME}
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ day06-spring-cloud-zuul/src/main/java/com/qfedu/cloud/ZuulApplication.java
@@ -0,0 +1,19 @@
+package com.qfedu.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+/**
+ * TODO 类描述
+ *
+ * @author LiJH.
+ * @date 2023/3/1 14:16
+ */
+@SpringBootApplication
+@EnableZuulProxy
+public class ZuulApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ZuulApplication.class, args);
+    }
+}
